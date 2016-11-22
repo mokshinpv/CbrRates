0adda5e3a64a4c559afa49788d39b8b04c788dc8
mokshinpv <mokshinpv@gmail.com>
2016-11-22 19:22:29 +0700

    Add project files.

---
+++ CbrRates/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CbrRates
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
